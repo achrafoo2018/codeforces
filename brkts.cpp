89238331c9e4aaedf24950da4f4a3034a52c32d0
Achraf Ben Soltane <achrafb.s2015@gmail.com>
2023-02-05 19:29:15 +0100

    new things

---
+++ brkts.cpp
@@ -0,0 +1,150 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<vll> vvll;
+typedef pair<ll, ll> pll;
+
+#define endl '\n';
+#define yes cout << "YES" << endl;
+#define no  cout << "NO" << endl;
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+mt19937_64 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define MP make_pair
+#define PB push_back
+#define fastio ios_base::sync_with_stdio(false)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define For(i, n) for(int i=0; i < n; ++i)
+#define dd(arr) For(i, arr.size()) cout << arr[i] << " "; cout << endl;
+
+
+struct node{
+	int opening = 0, closing = 0;
+	bool 
+};
+template <class Array_Type, class Tree_Type>
+class segment_tree{
+public:
+	int size;
+	vector<Tree_Type> tree;
+	void init(int n){
+		size = 1;
+		while(size < n) size *= 2;
+		tree.resize(size * 2);
+	}
+ 
+	void build(string &arr){
+        init(arr.size());
+		build(arr, 0, 0, size);
+	}
+ 
+ 	void build(string &arr, int x, int lx, int rx){
+		if(rx - lx == 1){
+			if(lx < arr.size()){
+				tree[x].opening = (arr[lx] == '(');
+				tree[x].closing = (arr[lx] == ')');
+			}
+			return;
+		}
+		int m = (lx + rx) / 2;
+		build(arr, 2*x+1, lx, m);
+		build(arr, 2*x+2, m, rx);
+		tree[x] = merge(x);
+	}
+ 
+	Tree_Type query(int l, int r){
+		return query(l, r, 0, 0, size);
+	}
+
+ 	Tree_Type query(int l, int r, int x, int lx, int rx){
+		if(lx >= r || l >= rx) return 0;
+		if(lx >= l && rx <= r) return tree[x];
+		int m = (lx + rx) / 2;
+		return query(l, r, 2*x+1, lx, m) + query(l, r, 2*x+2, m, rx);
+	}
+
+	void update(int i){
+		update(i, v, 0, 0, size);
+	}
+ 
+	void update(int i, int x, int lx, int rx){
+		if(rx - lx == 1){
+			tree[x].opening = !tree[x].opening;
+			tree[x].closing = !tree[x].closing;
+			return;
+		}
+		int m = (lx + rx) / 2;
+		if(i < m){
+			update(i, v, 2*x+1, lx, m);
+		}else{
+			update(i, v, 2*x+2, m, rx);
+		}
+		tree[x] = merge(x);
+	}
+
+	Tree_Type merge(int x){
+		Tree_Type left = tree[2*x+1];
+		Tree_Type right = tree[2*x+2];
+		Tree_Type ret = node();
+		ret.opening = left.opening + right.opening - left.closing;
+		ret.closing = left.closing + right.closing - left.opening;
+		return left + right;
+	}
+};
+
+
+void solve(){
+	string s; cin >> s;
+	segment_tree<char, node> st;
+	st.build(s);
+
+}
+
+int main(){
+	fastio;
+	// cout << setprecision(9);
+	// cout << fixed;
+	// freopen("input.in", "r", stdin);
+    // freopen("output.out", "w", stdout);
+    int t = 1;
+	// cin >> t;
+	while(t--)
+        solve();
+	return 0;
+}
